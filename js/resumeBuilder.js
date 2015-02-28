90e2cdc91423addefec453236816487fccb8100c
Matt Byrne <matt.byrne@yahoo.co.uk>
2015-02-28 20:44:29 +0000

    awesome Javascript

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,24 @@
 var awesomeThoughts = "I am totally awesome!";
+
+var funThoughts = awesomeThoughts.replace("awesome","Fun");
+
+console.log(awesomeThoughts);
+console.log(funThoughts);
+
+//$("#main").append(funThoughts);
+var myName = "Matthew Byrne";
+var formattedName = HTMLheaderName.replace("%data%",myName);
+
+var myRole = "Front-end Ninja";
+var formattedRole = HTMLheaderRole.replace("%data%",myRole);
+
+$("#main").prepend(formattedRole);
+$("#main").prepend(formattedName);
+
+//"audacity" to "Udacity".
+var wrong = "audacity";
+
+var firstCharUpper = wrong.charAt(1).toUpperCase();
+var right = firstCharUpper +  wrong.slice(2,wrong.length);
+
+console.log(right);
